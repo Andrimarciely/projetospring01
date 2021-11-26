bf9be7dd3595e5d2956a309e9577e86f97d80fa5
Andrimarciely <2019003790@ifam.edu.br>
2021-11-26 16:41:12 -0400

    Initial commit

---
+++ src/main/java/edu/ifam/tads/frameworks/projetospring01/controller/HomeController.java
@@ -0,0 +1,10 @@
+package edu.ifam.tads.frameworks.projetospring01.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    public String index(){
+        System.out.println("Funcionou!")
+    }
+}
