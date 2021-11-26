ab001b19aaa07bc97e247ea77ba04b364b20b182
Andrimarciely <2019003790@ifam.edu.br>
2021-11-26 17:56:29 -0400

    New feat - retorna um array de Pessoa.

--- src/main/java/edu/ifam/tads/frameworks/projetospring01/controller/PessoaController.java
+++ src/main/java/edu/ifam/tads/frameworks/projetospring01/controller/PessoaController.java
@@ -6,15 +6,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PessoaController {
     @RequestMapping(value = "/pessoa/find", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Pessoa find(){
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Maria");
-        pessoa.setTelefone("91234-5678");
-        pessoa.setEmail("maria@email.com");
+    public List<Pessoa> find(){
+        List<Pessoa> pessoa = new ArrayList<>();
+
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.setNome("Maria");
+        pessoa1.setTelefone("91234-5678");
+        pessoa1.setEmail("maria@email.com");
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setNome("Maria");
+        pessoa2.setTelefone("91234-5678");
+        pessoa2.setEmail("maria@email.com");
+
+        pessoa.add(pessoa1);
+        pessoa.add(pessoa2);
+
         return pessoa;
 
     }
