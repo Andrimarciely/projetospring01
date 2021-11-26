7a6cb87ccff619e72a066863c1e44609f8d4f54b
Andrimarciely <2019003790@ifam.edu.br>
2021-11-26 16:51:15 -0400

    New feat - Resposta no browser

--- src/main/java/edu/ifam/tads/frameworks/projetospring01/controller/HomeController.java
+++ src/main/java/edu/ifam/tads/frameworks/projetospring01/controller/HomeController.java
@@ -1,9 +1,13 @@
 package edu.ifam.tads.frameworks.projetospring01.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
     public String index(){
         System.out.println("Funcionou!");
         return "Funcionou no Browser";
